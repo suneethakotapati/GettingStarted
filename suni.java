cc9e26707c8fd0befeef4c509eb5991ed61e8ffe
suneethakotapati <98263058+suneethakotapati@users.noreply.github.com>
2022-07-19 09:17:12 +0530

    Add files via upload

---
+++ suni.java
@@ -0,0 +1,21 @@
+
+    class suni{
+        public static void main(String[] args){
+            int num1=100;
+            int num2=200;
+            if(num1>num2){
+                System.out.println("num1 is bigger!!");
+    
+            }
+            else if{
+                System.out.println("num1 is lesser");
+            }
+            else{
+                System.out.println("error");
+
+            }
+
+        }
+    
+    
+    }
